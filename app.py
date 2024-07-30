59b14fa9fa32f5ce8c314528ad99189a28e567d5
muhamadgibran <muhamadgibran751@gmail.com>
2024-07-30 11:37:26 +0700

    update environment variables

--- app.py
+++ app.py
@@ -124,8 +124,8 @@ class Server(BaseApplication):
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     #options = {
     #    'bind': '0.0.0.0:5000',
     #    'workers': 4 
